147c61a96ba761c40be6ca852a6955dc8d952e2f
Peer Springstübe <kratenko@garstig.org>
2019-12-09 13:21:01 +0100

    Version 0.2.3, preparing for 2nd shipping to KVB.

--- src/coap_main.h
+++ src/coap_main.h
@@ -32,9 +32,9 @@
 
 #define USE_RFC7641_ADVANCED_TRANSMISSION (1) //Update representation of resource during retry of observe sendout
 
-#define ACK_TIMEOUT (8)
+#define ACK_TIMEOUT (4)
 #define ACK_RANDOM_FACTOR (1.5)
-#define MAX_RETRANSMIT (4)
+#define MAX_RETRANSMIT (3)
 #define NSTART (1) //todo implement
 #define DEFAULT_LEISURE (5) //todo implement
 #define PROBING_RATE (1)        //[client]
